a4b3a6168ec54cdf3b34677aeaa27ce45bd6f5ce
avalynndev <avalynndev@gmail.com>
2022-12-31 09:21:34 +0530

    testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
   const [visit, setVisit] = useState(0);
   useEffect(() => {
     countapi.update("yt2mp3", "visits", 1).then((result) => {
-      console.log(result.value);
       setVisit(result.value);
     });
   }, []);
