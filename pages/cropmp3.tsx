c56c704e6134e91562b9096951bf4967e2d4e709
avalynndev <avalynndev@gmail.com>
2023-01-08 14:15:50 +0530

    Update cropmp3.tsx

--- pages/cropmp3.tsx
+++ pages/cropmp3.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <>
-     <h1>hello there</h1>
+     <Navbar />
     </>
   );
 }
