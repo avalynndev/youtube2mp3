f28daa48781cb0f641f7ac3e53886cf37a53960d
avalynndev <avalynndev@gmail.com>
2023-01-08 13:08:14 +0530

    test

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,15 +7,21 @@ function HeroSection() {
         Responsive Navbar using <span className="text-blue-500">Next.js</span> &{" "}
         <span className="text-blue-400">Tailwind</span>
       </h1>
-	  <input
-            type="text"
-            id="first_name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            required
-          />
+      <form>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <input
+              type="text"
+              id="first_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder=""
+              required
+            />
+          </div>
+        </div>
+      </form>
       <a href="">
-        <div className="flex justify-center items-center cursor-pointer hover:shadow-lg  text-3xl font-semibold text-white bg-gray-900 rounded-lg w-64 h-16"> 
+        <div className="flex justify-center items-center cursor-pointer hover:shadow-lg  text-3xl font-semibold text-white bg-gray-900 rounded-lg w-64 h-16">
           <h1 className="text-center">Github 🚀 </h1>
         </div>
       </a>
